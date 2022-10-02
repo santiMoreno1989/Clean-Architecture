f342c0b3b30671a4c684d44693528ced3c3adbcb
santiMoreno1989 <santiagom@gmail.com>
2022-10-02 20:37:41 -0300

    dto para crear un estudiante

---
+++ CleanArchitecture/CleanArchitecture.Application/Common/Dtos/EstudianteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Application.Common.Dtos
+{
+    public class EstudianteRequest
+    {
+        [Required]
+        [MaxLength(200,ErrorMessage ="Ha excedido el maximo de 200 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(250, ErrorMessage = "Ha excedido el maximo de 250 caracteres.")]
+        public string Apellido { get; set; } = string.Empty;
+
+    }
+}
