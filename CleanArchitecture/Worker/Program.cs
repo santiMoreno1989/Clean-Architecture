3c08841ee4e07ee9c8c264b8e3ff795d500e4d35
santiMoreno1989 <santiagom@gmail.com>
2023-11-10 16:27:10 -0300

    Se agrego proyecto de consola para escalamiento horizontal para hangfire.

---
+++ CleanArchitecture/Worker/Program.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(cfg => 
+{
+	cfg.UseSqlServerStorage("Data Source=LOST-LEGACY\\SQLEXPRESS;Initial Catalog=HangfireTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+});
+builder.Services.AddHangfireServer(options=> 
+{
+	options.WorkerCount = 30;
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseHangfireDashboard();
+
+app.Run();
